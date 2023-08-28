fb4257e64bf0a62ac1aea59698f6470237b42d19
gregory <greg2dl101@yahoo.com>
2023-08-28 23:59:15 +0200

    completed exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const punctuation = /[\.,?!\s]/g;
+  str = str.replace(punctuation, "");
+  str = str.toLowerCase();
+  const len = str.length;
 
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 // Do not edit below this line
