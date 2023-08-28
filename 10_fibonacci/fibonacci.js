432a15fd7653150f9ad7063be53cba1a511be0ad
gregory <greg2dl101@yahoo.com>
2023-08-29 00:10:42 +0200

    completed exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n >= 1) {
+    let a = 0,
+      b = 1,
+      c,
+      i;
+    if (n == 0) return a;
+    for (i = 2; i <= n; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
+  } else {
+    return "OOPS";
+  }
 };
 
 // Do not edit below this line
